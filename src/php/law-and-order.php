b542de0549e1026025e63f011f2bee10c2fa3c9a
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-06-01 20:54:02 +0200

    building error messages

--- src/php/law-and-order.php
+++ src/php/law-and-order.php
@@ -18,15 +18,13 @@
  */
 
 /**
- * Description of law-and-order
+ * This class contains functions to check rosters for adherence to legal requirements.
  *
  * @author Mandelkow
  */
 class law_and_order {
 
-//This class should be used to check plans for adherence to legal requirements
-
-    public function check_maximum_working_hours($date_sql) {
+    public function check_maximum_working_hours($date_sql, $employee_id) {
         /*
          * Germany
          * Arbeitszeitgesetz (ArbZG)
@@ -35,14 +33,33 @@ class law_and_order {
          * Sie kann auf bis zu zehn Stunden nur verlängert werden,
          *  wenn innerhalb von sechs Kalendermonaten oder innerhalb von 24 Wochen im Durchschnitt acht Stunden werktäglich nicht überschritten werden.
          */
-        $sql_query = "SELECT SUM(`Stunden`) AS `sum_of_hours` FROM `dienstplan` WHERE `Datum` BETWEEN DATE($date_sql) - INTERVAL 24 WEEK AND DATE($date_sql)";
+        $sql_query = "SELECT SUM(`Stunden`) AS `sum_of_hours`"
+                . " FROM `dienstplan`"
+                . " WHERE `Datum` BETWEEN DATE($date_sql) - INTERVAL 24 WEEK AND DATE($date_sql)"
+                . " AND `VK` = '$employee_id'";
         $row = mysqli_query_for_single_object($sql_query);
         $sum_of_hours = $row->sum_of_hours;
+        $sum_of_days = 0;
         for ($date_unix = strtotime($date_sql); $date_unix > strtotime("-24 weeks", strtotime($date_sql)); $date_unix = strtotime("-1 day", $date_unix)) {
-            if (0 != strftime("%w", $date_unix) and !  is_holiday($date_unix)) {
+            if (0 != strftime("%w", $date_unix) and ! is_holiday($date_unix)) {
                 $sum_of_days++;
+                print_debug_variable(date('YYYY-mm-dd', $date_unix));
             }
         }
+        $average_working_hours = $sum_of_hours / $sum_of_days;
+        if ($average_working_hours > 8) {
+            return build_error_message_maximum_working_hours($average_working_hours, $employee_id);
+        }
+    }
+
+    private function build_error_message_maximum_working_hours($average_working_hours, $employee_id) {
+        global $Mitarbeiter;
+        $error_message = $Mitarbeiter[$employee_id] . " arbeitet im Durchschnitt " . $average_working_hours 
+                . " das ist ein Verstoß gegen <a href='http://www.gesetze-im-internet.de/arbzg/__3.html'>§3 ArbZG</a>!";
+        if (!function_exists(build_warning_messages)) {
+            require_once 'src/php/build-warning-messages.php';
+        }
+        return build_warning_messages($error_message);
     }
 
 }
